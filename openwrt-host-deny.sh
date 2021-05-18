61da7869725aeed0568133a3945187943282da8e
rongge <rongge@newborntown.com>
2021-05-18 17:50:07 +0800

    update openwrt封禁ssh黑名单脚本

--- openwrt-host-deny.sh
+++ openwrt-host-deny.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 num=2
-for  i in `logread   | grep Invalid | cut -f12 -d' ' | sort | uniq -c| sort -rn| awk '{if ($1 > $num){print $2}}'`
+for  i in `logread   | grep Disconnected | cut -f13 -d' ' | sort | uniq -c| sort -rn| awk '{if ($1 > $num){print $2}}'`
 do
 	ipset test blacklist $i
 	if [ $? -ne 0 ]; then
