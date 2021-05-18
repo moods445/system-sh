680cc87e6865c70fef02875d3c430e6f03e10aff
rongge <rongge@newborntown.com>
2021-05-18 17:36:13 +0800

    update openwrt封禁ssh黑名单脚本

--- openwrt-host-deny.sh
+++ openwrt-host-deny.sh
@@ -2,8 +2,9 @@
 num=2
 for  i in `logread   | grep Invalid | cut -f12 -d' ' | sort | uniq -c| sort -rn| awk '{if ($1 > $num){print $2}}'`
 do
-	ipset test blacklist $i | grep -i not
-	if [ $? -eq 0 ]; then
+	ipset test blacklist $i
+	if [ $? -ne 0 ]; then
+       echo "into blacklist"
 	   echo $i >> /var/log/deny.log
 	   ipset add blacklist $i
    fi
